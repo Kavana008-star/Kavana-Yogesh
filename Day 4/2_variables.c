646e24a0a1b76f86041d328223f5da06e283284b
unknown <snehanilankar1806gmail.com>
2025-11-20 15:11:48 +0530

    day 4 code pushed

---
+++ Day 4/2_variables.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    int age=30;
+    printf("your age is:%d \n",age);
+
+    float marks=175.16;
+    printf("your marks is: %f \n",marks);  
+char name='k';
+printf("my name is:%c \n",name);
+double pi=3.14;
+printf("pi value:%lf \n",pi);
+bool iswalking=true;
+printf("iswalking:%d \n",iswalking);
+
+}
